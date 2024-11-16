d597dde5234c77f1d967e033ee07ba34e2760e2b
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2024-11-16 11:35:59 +0530

    Add files via upload

---
+++ server/models/Media.js
@@ -0,0 +1,12 @@
+// server/models/Media.js
+
+
+const mongoose = require('mongoose');
+
+const mediaSchema = new mongoose.Schema({
+    url: { type: String, required: true },
+    type: { type: String, required: true },
+    uploadedAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Media', mediaSchema);
